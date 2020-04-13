13665905ca451393aa6d37fb74c861ba754dbc74
Dimas <dimasbecker@outlook.com>
2020-04-13 20:39:19 -0300

    nuevo commit

--- DDL/Tabla.sql
+++ DDL/Tabla.sql
@@ -1,28 +1,19 @@
 
-create table cuenta(
-cue_numero char(2),
-cue_subcuenta char(4),
-cue_descripcion varchar(50)
-constraint Pk_Cuenta primary key (cue_numero,cue_subcuenta)
+--tabla ejemplo
+CREATE TABLE pruebaGit
+(
+    id int NOT NULL,
+    nombre varchar(50),
+    edad int
 )
 
-create table Movimiento(
-id_movimiento int not null identity(1,1),
-cue_numero char(2),
-cue_subcuenta char(4),
-debito int null, 
-credito int null, 
-mov_descripcion varchar(50),
-mov_fecha datetime,
+--tabla auditoria
+CREATE TABLE auditoria
+(
+    id int NOT NULL,
+    fecha DATETIME,
+    tabla varchar(50),
+    id_afectado int,
+    tipo_accion varchar(50),
+    usuario varchar(50)
 )
-
-
-create table Resumen(
-cue_numero char(2),
-cue_subcuenta char(4),
-debito int null, 
-credito int null, 
-fecha_calculo datetime,
-constraint Fk_Resumen foreign key (cue_numero,cue_subcuenta) references cuenta (cue_numero,cue_subcuenta)
-)
-
